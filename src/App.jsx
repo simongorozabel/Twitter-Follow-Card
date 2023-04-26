d48a3135c27b9a1f142d7f95eb12b20621b5eadd
Simon Gorozabel <sgorozabel@gmail.com>
2023-04-26 04:27:32 -0500

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
+const users = [
+    {
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán',
+      isFollowing: true
+    },
+    {
+      userName: 'freddier',
+      name: 'Freddy Vega',
+      isFollowing: false
+    },
+    {
+      userName: 'simongorozabel',
+      name: 'Simón Gorozabel',
+      isFollowing: true
+    },
+    {
+      userName: 'elonmusk',
+      name: 'Elon Musk',
+      isFollowing: false
+    }
+  ]
+
 function App() {
 
   return (
-    <section className="App">
-      <TwitterFollowCard
-        initialIsFollowing={true}
-        userName="simongorozabel"
-        name="Simon Gorozabel"
-      />
-      <TwitterFollowCard
-        userName="midudev"
-        name="Miguel Ángel Durán"
-      />
-      <TwitterFollowCard
-        userName="freddier"
-        name="Freddy Vega"
-      />
-      <TwitterFollowCard
-        userName="elonmusk"
-        name="Elon Musk"
-      />
-    </section>
-  );
+    <section className='App'>
+    {
+      users.map(({ userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          name={name}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))
+    }
+  </section>
+)
 }
 
 export default App;
